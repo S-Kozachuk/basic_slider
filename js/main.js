98e681a99375d9c428a08600b02521454e6b9562
S-Kozachuk <qjT7E7ZLQkUGxOZY00MI>
2022-01-20 10:58:54 +0300

    SLD/JS/Added button class with variables

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ const btnL = document.getElementById('btnL');
 const btnR = document.getElementById('btnR');
 const slider = document.querySelector('.slider-wrapper');
 let offset = 0;
+
+function activ () {
+	btnL.className = "slider-prev";
+	btnR.className = "slider-next";
+}
