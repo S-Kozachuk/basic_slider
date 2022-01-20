583ac4722fd43888804665e2fc867d08d5b013a0
S-Kozachuk <qjT7E7ZLQkUGxOZY00MI>
2022-01-20 11:33:43 +0300

    SLD/JS/Added handler to right button(slider-next)

--- js/main.js
+++ js/main.js
@@ -10,9 +10,20 @@ function activ () {
 
 btnL.addEventListener('click', () => {
 	btnL.className = "active";
-	offset -=105;
+	offset -= 105;
 	if (offset < 0){
-		offset=630;
+		offset = 630;
+	}
+
+	slider.style.left = -offset + 'px';
+	setTimeout(activ, 400);
+});
+
+btnR.addEventListener('click', () => {
+	btnR.className = "active";
+	offset += 105;
+	if (offset > 630){
+		offset = 0;
 	}
 
 	slider.style.left = -offset + 'px';
